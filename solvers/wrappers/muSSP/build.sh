8d3782d1e75c4250a8a6e2ece830e31eb239e12e
Leonardo Citraro <leonardo.citraro@epfl.ch>
2021-03-19 11:09:30 +0100

    better error signal in build

--- solvers/wrappers/muSSP/build.sh
+++ solvers/wrappers/muSSP/build.sh
@@ -2,6 +2,11 @@ rm -rdf build
 mkdir build
 cd build
 cmake ..
+exit_code=$?
+if [ "$exit_code" -ne 0 ]; then
+    >&2 echo "cmake failed with exit code ${exit_code}."
+    exit $exit_code
+fi
 make
 cp *.so ..
 cd ..
